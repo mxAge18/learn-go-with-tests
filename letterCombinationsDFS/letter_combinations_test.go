7c9554027d32e3aab0c173f3ed6d909385a339c1
invokerx <maxu0410@163.com>
2021-09-08 00:46:22 +0100

    add comment

--- letterCombinationsDFS/letter_combinations_test.go
+++ letterCombinationsDFS/letter_combinations_test.go
@@ -9,6 +9,7 @@ func TestLetterCombinations(t *testing.T) {
 	expected := []string{"ad","ae","af","bd","be","bf","cd","ce","cf"}
 	res := LetterCombinations("23")
 	
+	// 比较两个slice不能直接比较
 	if !reflect.DeepEqual(expected, res) {
 		t.Errorf("expected '%s' got '%s'", expected, res)
 	}
