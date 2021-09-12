9ddc5e7fc5877df5f4a15f3e5c59329359d5dd8f
invokerx <maxu0410@163.com>
2021-09-13 01:26:26 +0800

    post new data

---
+++ http_server/v2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+    return 123
+}
+func (i *InMemoryPlayerStore) RecordWin(name string) {}
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	err := http.ListenAndServe(":5000", server)
+	if err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
