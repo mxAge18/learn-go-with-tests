a526d5bd6e52203a96d2e4d5302e5b6061eb991b
invokerx <maxu0410@163.com>
2021-09-12 13:55:16 +0800

    use spy to check sleep and write's order

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -22,6 +22,13 @@ type ConfigurableSleeper struct {
 func (o *ConfigurableSleeper) Sleep() {
     time.Sleep(o.duration)
 }
+// Countdown 应该在第一个打印之前 sleep，然后是直到最后一个前的每一个，例如：
+// Sleep
+// Print N
+// Sleep
+// Print N-1
+// Sleep
+// etc 测试时执行的顺序无法确定
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
@@ -30,6 +37,18 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	sleeper.Sleep()
     fmt.Fprint(out, finalWord)
 }
+// func Countdown(out io.Writer, sleeper Sleeper) {
+//     for i := countdownStart; i > 0; i-- {
+//         sleeper.Sleep()
+//     }
+
+//     for i := countdownStart; i > 0; i-- {
+//         fmt.Fprintln(out, i)
+//     }
+
+//     sleeper.Sleep()
+//     fmt.Fprint(out, finalWord)
+// }
 
 func main() {
 	sleeper := &ConfigurableSleeper{1 * time.Second}
