584f98290278d5d9ddbffae4289c3a4a89e6f15a
invokerx <maxu0410@163.com>
2021-09-14 19:23:50 +0800

    debug

--- command-line/v3/cmd/cli/main.go
+++ command-line/v3/cmd/cli/main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
-	poker "github.com/mxage18/learn-go-with-tests/cmd/v3"
+	poker "github.com/mxage18/learn-go-with-tests/command-line/v3"
 )
 
 const dbFileName = "game.db.json"
 func main() {
+	
 	store, err := poker.FileSystemStoreFromFile(dbFileName)
-
+	
+	fmt.Println(GetAppPath())
   	if err != nil {
        log.Fatal(err)
     }
@@ -19,4 +24,11 @@ func main() {
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
 	poker.NewCLI(store, os.Stdin).PlayPoker()
+}
+
+func GetAppPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+	return path[:index]
 }
